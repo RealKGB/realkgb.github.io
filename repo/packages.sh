c25fff049a69d4098e45f6b571ed290d9e1a0496
WhitetailAni <87622301+RealKGB@users.noreply.github.com>
2022-11-22 21:57:51 -0600

    email update v2

--- repo/packages.sh
+++ repo/packages.sh
@@ -1,4 +1,9 @@
 rm Packages.gz
 rm Packages.bz2
+rm Packages.lz4
+rm Packages.lzma
+sleep 1.5
 gzip -c9 Packages > Packages.gz
 bzip2 -c9 Packages > Packages.bz2
+lzma -c9 Packages > Packages.lzma
+lz4 Packages
